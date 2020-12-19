f065fa091ef2dcf9ebae0ed7d067712cf7c7434a
sergey.binder <fornewreal@gmail.com>
2020-12-19 21:07:38 +0700

    GraphicsOrderer can order meshes

--- Assets/Scripts/Behaviour/Utils/GraphicsOrderer.cs
+++ Assets/Scripts/Behaviour/Utils/GraphicsOrderer.cs
@@ -4,41 +4,69 @@ using System.Collections.Generic;
 
 namespace SmtProject.Behaviour.Utils {
 	public sealed class GraphicsOrderer : MonoBehaviour {
+		interface IOrderable {
+			void Reorder(int index);
+		}
+
+		abstract class BaseOrderable<T> : IOrderable {
+			protected readonly T Orderable;
+
+			protected BaseOrderable(T orderable) {
+				Orderable = orderable;
+			}
+
+			public abstract void Reorder(int index);
+		}
+
+		sealed class SpriteRendererOrderable : BaseOrderable<SpriteRenderer> {
+			public SpriteRendererOrderable(SpriteRenderer orderable) : base(orderable) { }
+
+			public override void Reorder(int index) {
+				Orderable.sortingOrder = index;
+			}
+		}
+
+		sealed class MeshRendererOrderable : BaseOrderable<MeshRenderer> {
+			public MeshRendererOrderable(MeshRenderer orderable) : base(orderable) { }
+
+			public override void Reorder(int index) {
+				Orderable.sortingOrder = index;
+			}
+		}
+
 		public float Offset;
 
-		readonly List<SpriteRenderer> _spriteRenderers = new List<SpriteRenderer>();
+		readonly List<IOrderable> _orderables = new List<IOrderable>();
 
 		void Start() {
-			CollectSpriteRenderers();
+			CollectOrderables();
 		}
 
-		void CollectSpriteRenderers() {
-			_spriteRenderers.Clear();
-			CollectSpriteRenderers(transform);
+		void CollectOrderables() {
+			_orderables.Clear();
+			CollectOrderables(transform);
 		}
 
-		void CollectSpriteRenderers(Transform tr) {
+		void CollectOrderables(Transform tr) {
 			var sr = tr.GetComponent<SpriteRenderer>();
 			if ( sr ) {
-				_spriteRenderers.Add(sr);
+				_orderables.Add(new SpriteRendererOrderable(sr));
 			}
+
+			var mr = tr.GetComponent<MeshRenderer>();
+			if ( mr ) {
+				_orderables.Add(new MeshRendererOrderable(mr));
+			}
+
 			for ( var i = 0; i < tr.childCount; ++i ) {
-				CollectSpriteRenderers(tr.GetChild(i));
+				CollectOrderables(tr.GetChild(i));
 			}
 		}
 
 		void Update() {
 			var startIndex = Mathf.CeilToInt(-(transform.position.y + Offset) * 1000f);
-			foreach ( var sr in _spriteRenderers ) {
-				sr.sortingOrder = startIndex++;
-			}
-		}
-
-		[ContextMenu("Update")]
-		void EditorUpdate() {
-			var startIndex = Mathf.CeilToInt(-(transform.position.y + Offset) * 1000f);
-			foreach ( var sr in GetComponentsInChildren<SpriteRenderer>() ) {
-				sr.sortingOrder = startIndex++;
+			foreach ( var orderable in _orderables ) {
+				orderable.Reorder(startIndex++);
 			}
 		}
 	}
