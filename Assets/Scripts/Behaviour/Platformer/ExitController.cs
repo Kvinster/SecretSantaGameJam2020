23e8a4a08565b235663d1cb20a132fecaa2c2753
sergey.binder <fornewreal@gmail.com>
2020-12-21 00:59:11 +0700

    Polish for itch builds

---
+++ Assets/Scripts/Behaviour/Platformer/ExitController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SmtProject.Behaviour.Platformer {
+	public sealed class ExitController : MonoBehaviour {
+		void Update() {
+#if !UNITY_WEBGL
+			if ( Input.GetKeyDown(KeyCode.Escape) ) {
+				Application.Quit();
+			}
+#endif
+		}
+	}
+}
